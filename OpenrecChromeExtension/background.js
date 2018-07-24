bdd615aad75a5a70f50009aaf3d7fae59f42b085
Shuntaro Tashiro <2093057+ukkary@users.noreply.github.com>
2018-07-24 16:08:49 +0900

    Update background.js

--- OpenrecChromeExtension/background.js
+++ OpenrecChromeExtension/background.js
@@ -1,18 +1,20 @@
 var old_text;
 
 function getLastChat() {
- return $('chat-content').last().text();
+ var elements = document.getElementsByClassName('chat-content');
+ return elements[elements.length -1].textContent;
 }
 
 
 $('.chat-list-content').on('DOMSubtreeModified propertychange', function(e) {
   var text = getLastChat();
-  if (ext.length == 0 || old_text == text) {
+  if (text.length == 0 || old_text == text) {
     return;
   }
- 
+
+  old_text = text;
   var socket = new WebSocket('ws://localhost:50002/');
   socket.onopen = function() {
     socket.send(text);
   }
-}
+});
